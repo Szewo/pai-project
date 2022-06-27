1581a40fe89e4a0dee8ce46c732c567a3b9b0282
Szewo <szewczyk.mat@yahoo.com>
2022-06-27 12:00:22 +0200

    Add dashboard view styles.

--- public/views/dashboard.php
+++ public/views/dashboard.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="../css/dashboard.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GymBro</title>
+    <?php include 'base.php' ?>
 
 </head>
 <body>
-<h1>GymBro Dashboard</h1>
-<?php include 'dashboard_navigation.php' ?>
+<div class="container">
+    <div class="navigation">
+        <i class="fa-solid fa-user user-avatar"></i>
+        <h3 class="user-mail"> <?php echo $_SESSION['email'] ?> </h3>
+        <button type="submit" class="sign-up-button">USER SETTINGS</button>
+        <?php include 'dashboard_navigation.php' ?>
+    </div>
+
+    <div class="content-wrapper">
+        <div class="header">
+            <span class="nav-text-first">GymBro</span> <span class="nav-text-second">Dashboard</span>
+        </div>
+        <div class="content">
+            <h1>Homepage</h1>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
--- public/views/dashboard_navigation.php
+++ public/views/dashboard_navigation.php
@@ -1,5 +1,22 @@
-<a href="<?php echo "http://$_SERVER[HTTP_HOST]/add-workout"?>">add workout</a> <br>
-<a href="<?php echo "http://$_SERVER[HTTP_HOST]/all-workouts"?>">all workouts</a> <br>
-<a href="<?php echo "http://$_SERVER[HTTP_HOST]/done-workouts"?>">done workouts</a> <br>
-<a href="<?php echo "http://$_SERVER[HTTP_HOST]/scheduled-workouts"?>">scheduled workouts</a> <br>
-<a href="<?php echo "http://$_SERVER[HTTP_HOST]/logout"?>">logout</a> <br>
+<ul class="nav-ul">
+    <li>
+        <i class="fa-solid fa-plus"></i>
+        <a class='button' href="<?php echo "http://$_SERVER[HTTP_HOST]/add-workout"?>">Add workout</a> <br>
+    </li>
+    <li>
+        <i class="fa-solid fa-list"></i>
+        <a class='button' href="<?php echo "http://$_SERVER[HTTP_HOST]/all-workouts"?>">All workouts</a> <br>
+    </li>
+    <li>
+        <i class="fa-solid fa-list-check"></i>
+        <a class='button' href="<?php echo "http://$_SERVER[HTTP_HOST]/done-workouts"?>">Done workouts</a> <br>
+    </li>
+    <li>
+        <i class="fa-solid fa-list-ol"></i>
+        <a class='button' href="<?php echo "http://$_SERVER[HTTP_HOST]/scheduled-workouts"?>">Scheduled workouts</a> <br>
+    </li>
+    <li>
+        <i class="fa-solid fa-right-from-bracket"></i>
+        <a class='button' href="<?php echo "http://$_SERVER[HTTP_HOST]/logout"?>">Logout</a> <br>
+    </li>
+</ul>
