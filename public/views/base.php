5a391e301e71bd3d31a1f371f346a5a909073023
Szewo <szewczyk.mat@yahoo.com>
2022-06-27 09:13:51 +0200

    Add login view styles.

---
+++ public/views/base.php
@@ -0,0 +1,4 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/9d4825b4f1.js" crossorigin="anonymous"></script>
--- public/views/login.php
+++ public/views/login.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
+    <?php include 'base.php' ?>
 </head>
 <body>
     <div class="base-container">
@@ -13,28 +14,33 @@
             <h2>Personal workout planner</h2>
         </div>
         <div class="sign-in-up-form">
-            <div class="message">
-                <?php
-                if(isset($message))
-                {
-                    echo $message;
-                }
-                ?>
+            <div class="login-form">
+                <i class="fa-solid fa-unlock login-icon"></i>
+                <h1>Sign In</h1>
+                <h4>Welcome to GymBro app.</h4>
+                <div class="message">
+                    <?php
+                    if(isset($message))
+                    {
+                        echo $message;
+                    }
+                    ?>
+                </div>
+                <form class="login" action="/" method="POST">
+                    <label>
+                        <input name="email" type="text" placeholder="Email"/>
+                    </label>
+                    <br>
+                    <label>
+                        <input name="password" type="password" placeholder="Password">
+                    </label>
+                    <br>
+                    <button type="submit" class="sign-in-button"> SUBMIT </button>
+                </form>
+                <form action="/register" method="get">
+                    <button type="submit" class="sign-up-button">REGISTER NEW ACCOUNT</button>
+                </form>
             </div>
-            <form class="login" action="/" method="POST">
-                <label>
-                    <input name="email" type="text" placeholder="email"/>
-                </label>
-                <br>
-                <label>
-                    <input name="password" type="password" placeholder="password">
-                </label>
-                <br>
-                <button type="submit"> SUBMIT </button>
-            </form>
-            <form class="sign_up" action="/register" method="get">
-                <button type="submit">REGISTER NEW ACCOUNT</button>
-            </form>
         </div>
     </div>
 </body>
