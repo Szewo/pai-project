c2392dfb18fc61254e3071b74b6ba22b6f30a191
Szewo <szewczyk.mat@yahoo.com>
2022-06-28 13:44:20 +0200

    Add view-workout styles.

--- public/views/view-workout.php
+++ public/views/view-workout.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="../css/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../css/view_workouts.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GymBro</title>
+    <?php include 'base.php' ?>
 
 </head>
 <body>
-<h1>GymBro Dashboard</h1>
-<?php include 'dashboard_navigation.php' ?>
-<?php if (isset($workout)): ?>
-    <?php echo $workout->getId() ?> <br>
-    <?php echo $workout->getName() ?> <br>
-    <?php echo $workout->getDate()->format('Y-m-d') ?> <br>
-<?php endif; ?>
+<div class="container">
+    <div class="navigation">
+        <i class="fa-solid fa-user user-avatar"></i>
+        <h3 class="user-mail"> <?php echo $_SESSION['email'] ?> </h3>
+        <button type="submit" class="sign-up-button">USER SETTINGS</button>
+        <?php include 'dashboard_navigation.php' ?>
+    </div>
+    <div class="content-wrapper">
+        <div class="header">
+            <span class="nav-text-first">GymBro</span> <span class="nav-text-second">Dashboard</span>
+        </div>
+        <div class="content">
+            <div class="workout-informations">
+                <?php if (isset($workout)): ?>
+                    Workout name: <?php echo $workout->getName() ?> <br>
+                    Workout date: <?php echo $workout->getDate()->format('Y-m-d') ?> <br>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
